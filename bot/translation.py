a8dea78a7c8b6f476e5f115adc8cdc093cb4678f
gujhan <82350696+gujhan@users.noreply.github.com>
2021-05-07 13:27:39 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @Tentkotta_Tamil
 
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+I have 4m movies🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+For More movies first update & Upload..
+@Tentkotta_Tamil
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Tentkotta_Tamil
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Thalapathy_off">Thalapathy_off</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Channel</b> : <i><a href="https://t.me/Tamil_Tentkotta">Tamil Tentkotta</a></i>
 """
